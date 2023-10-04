8752513a313827034d42eb288440b16765aa6897
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-10-04 14:46:31 -0700

    Add gameboard board generation

--- gameboard.js
+++ gameboard.js
@@ -1,5 +1,34 @@
 import Ship from "./ship";
 
 export default class Gameboard {
+  constructor(dimensions) {
+    this.board = this.generateBoard(dimensions);
+  }
 
+  generateBoard(dimensions) {
+    const board = [];
+
+    // Create array of rows
+    for (let i=0; i<dimensions; i+=1) {
+      const row = [];
+
+      // In each row, create array of cells
+      for (let j=0; j<dimensions; j+=1) {
+        const cell = new Cell(i, j);
+        row.push(cell);
+      }
+
+      board.push(row);
+    }
+
+    return board;
+  }
+}
+
+class Cell {
+  constructor(x, y) {
+    this.position = [x, y];
+    this.isEmpty = true;
+    this.beenHit = false;
+  }
 }
--- gameboard.test.js
+++ gameboard.test.js
@@ -3,9 +3,25 @@ import Gameboard from "./gameboard";
 let gameboard;
 
 beforeEach(() => {
-  gameboard = new Gameboard;
+  gameboard = new Gameboard(10);
 });
 
 test('Gameboard class creates an object', () => {
   expect(gameboard).toBeDefined();
 });
+
+test('Gameboard has board', () => {
+  expect(gameboard.board).toBeDefined();
+});
+
+test('Gameboard has 10 rows', () => {
+  expect(gameboard.board).toHaveLength(10);
+});
+
+test('Each Gameboard row has 10 cells', () => {
+  expect(gameboard.board[0]).toHaveLength(10);
+  expect(gameboard.board[3]).toHaveLength(10);
+  expect(gameboard.board[9]).toHaveLength(10);
+});
+
+test.todo('Each gameboard includes correct cells');
