db5f45e2edf155279f10853590b150b626154440
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-10-11 16:02:12 -0700

    Add player attack tests and functions

--- player.js
+++ player.js
@@ -1,3 +1,29 @@
+import Gameboard from "./gameboard";
+
 export default class Player {
-  
+  constructor() {
+    this.gameboard = new Gameboard(10);
+    this.previousMoves = [];
+    this.opponent = null;
+    this.myTurn = false;
+  }
+
+  setOpponent(opponent) {
+    this.opponent = opponent;
+  }
+
+  attackOpponent([x, y]) {
+    if (this.myTurn === true) {
+      // Validate coordinates are in bounds
+      const max = this.gameboard.dimensions;
+      if (x < 0 || x >= max || y < 0 || y >= max) {
+        throw new Error("Out of bounds");
+      }
+      this.myTurn = false;
+      this.opponent.myTurn = true;
+      return this.gameboard.receiveAttack([x, y]);
+    } else {
+      throw new Error("Not my turn!");
+    }
+  }
 }
--- player.test.js
+++ player.test.js
@@ -1,2 +1,64 @@
 import Player from './player';
 
+let playerOne;
+let playerTwo;
+
+beforeEach(() => {
+  playerOne = new Player();
+  playerTwo = new Player();
+  playerOne.setOpponent(playerTwo);
+  playerTwo.setOpponent(playerOne);
+  playerOne.myTurn = true;
+});
+
+describe("Player can attack opponent board", () => {
+  test('Player gets response for opponent attacks', () => {
+    expect(playerOne.attackOpponent([1, 2])).toEqual([false, false]);
+  });
+
+  test('Players cannot go twice in a row', () => {
+    playerOne.attackOpponent([5, 3]);
+    expect(() => {
+      playerOne.attackOpponent([3, 6]);
+    }).toThrow("Not my turn!");
+  });
+
+  test('Players can take turns', () => {
+    playerOne.attackOpponent([1, 2]);
+    playerTwo.attackOpponent([5, 5]);
+    expect(playerOne.attackOpponent([3, 5])).toEqual([false, false]);
+  });
+});
+
+describe("Prevent illegal moves", () => {
+  test("Disallow moves off the board (horizontal, too big)", () => {
+    expect(() => {
+      playerOne.attackOpponent([10, 2]);
+    }).toThrow("Out of bounds");
+  });
+
+  test("Disallow moves off the board (horizontal, too small)", () => {
+    expect(() => {
+      playerOne.attackOpponent([-5, 2]);
+    }).toThrow("Out of bounds");
+  });
+
+  test("Disallow moves off the board (vertical, too big)", () => {
+    expect(() => {
+      playerOne.attackOpponent([2, 10]);
+    }).toThrow("Out of bounds");
+  });
+
+  test("Disallow moves off the board (vertical, too small)", () => {
+    expect(() => {
+      playerOne.attackOpponent([2, -5]);
+    }).toThrow("Out of bounds");
+  });
+
+  test.todo("Disallow moves off the board (vertical)");
+  test.todo("Disallow duplicate moves");
+});
+
+describe("Game engine AI", () => {
+  test.todo("Game engine selects random moves");
+});
