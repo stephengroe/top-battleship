02896350442be949b96d2203714040e05109f83b
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-10-11 17:04:23 -0700

    Add initial game functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-import * from './ship';
+import Player from "./player";
+
+function initializeGame() {
+  const playerOne = new Player();
+  const playerTwo = new Player();
+  playerOne.setOpponent(playerTwo);
+  playerTwo.setOpponent(playerOne);
+  playerOne.myTurn = true;
+}
+
+// Initialize "new game" button
+const newGameButton = document.querySelector("#new-game-button");
+newGameButton.addEventListener("click", (event) => {
+  initializeGame();
+});
